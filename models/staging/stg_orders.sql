2c1b66c0a6885ec5af2a0d1164ceff21b1c41cdc
Alexander Eieland <alexander.kirkeby.eieland@computas.com>
2023-01-24 10:11:38 +0100

    Added dbtplyr package and updated models, .gitignore

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,7 +1,7 @@
 select
-        id as order_id,
-        customer as customer_id,
-        ordered_at as order_date
-        --,status
+    id as order_id,
+    customer as customer_id,
+    ordered_at as order_date,
+    'SHIPPED' as c_status
 
-    from `cx-datalab`.dbt_alei2.raw_orders
+from `cx-datalab`.dbt_alei2.raw_orders
